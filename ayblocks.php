429e0c8073b6e8c12a896940c7d73aca009b9642
asciito <ayaxdev.mx@gmail.com>
2022-09-26 10:22:17 -0500

    :sparkles: 'Hero Presentation' block added
    [x] Structure and source code for the block
    [x] Block type registered

--- ayblocks.php
+++ ayblocks.php
@@ -22,5 +22,6 @@
  */
 function create_block_ayblocks_block_init() {
 	// register_block_type( __DIR__ . '/build/template' );
+	register_block_type( __DIR__ . '/build/hero-presentation' );
 }
 add_action( 'init', 'create_block_ayblocks_block_init' );
